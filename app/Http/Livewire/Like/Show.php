5b16713aba4db5092a1819756542d00eb625b5e6
Sagar Chhetri <slaure354@gmail.com>
2021-05-01 23:15:16 +0545

    Create Show.php

---
+++ app/Http/Livewire/Like/Show.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\Like;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                This is just text for div in inline in livewire!
+            </div>
+        blade;
+    }
+}
